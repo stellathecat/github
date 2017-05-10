ba6a9ea82a219a3dd5fd509bbde49062dd06cf6c
stellathecat <Maschine@Viktors-MacBook-Air.local>
2017-05-10 17:50:53 +0200

    May
    May

--- test.R
+++ test.R
@@ -1,2 +1,5 @@
 require(xts)
+source(gzcon(url("https://raw.githubusercontent.com/stellathecat/github/master/martingale.R")))
 test=readRDS(gzcon(url("https://dl.dropboxusercontent.com/s/uo19beaq2lw0lii/EURCHF2016.rds"))) 
+
+martingale(test,'2015','2016',0.0015,100000,0,2,1)
