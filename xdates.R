c7e2f702b432460ff73f7230f177659519fdab0b
stellathecat <stellathecat@users.noreply.github.com>
2017-08-22 23:53:39 +0200

    123

---
+++ xdates.R
@@ -0,0 +1,7 @@
+finddates = function(x, type='BID') {
+today <- gsub('-','',format(Sys.Date(), "%Y-%m-%d %H:%M:%S"))
+if(type=='BID') dates <- as.Date(index(reqHistoricalData(tws,x,today,'1 week','15 Y','0','BID')))
+if(type=='OPT') dates <- as.Date(index(reqHistoricalData(tws,x,today,'1 week','15 Y','0','OPTION_IMPLIED_VOLATILITY')))
+dates <- seq(dates[1],Sys.Date(),by=1)
+dates[weekdays(dates) %in% 'Sunday']
+}
