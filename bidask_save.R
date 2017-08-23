dfc19e4e721f62f930de3332769d07cfa971a839
stellathecat <stellathecat@users.noreply.github.com>
2017-08-23 09:08:50 +0200

    Update bidask_save.R

--- bidask_save.R
+++ bidask_save.R
@@ -2,8 +2,8 @@ source(gzcon(url("https://raw.githubusercontent.com/stellathecat/github/master/c
 bidask <- function(y,x) {
     timexx <<- gsub('-','',format(y, "%Y-%m-%d %H:%M:%S")); print(paste(timexx,format(Sys.time(), "%X")))
     savexx <<- paste('BA',which(dates==y))
-    a <- savedl(reqHistoricalData(tws,x,timexx,'1 min','5 D','0','BID')[,c(1:4)]); counter()
-    b <- savedl(reqHistoricalData(tws,x,timexx,'1 min','5 D','0','ASK')[,c(1:4)]); counter()
+    a <- savedl(reqHistoricalData(tws,x,timexx,'1 min','5 D','0','BID')[,c(1:4)]); Sys.sleep(5); counter()
+    b <- savedl(reqHistoricalData(tws,x,timexx,'1 min','5 D','0','ASK')[,c(1:4)]); Sys.sleep(5); counter()
     yy <- cbind(a,b) # works because a and b do EXIST (but maybe NULL)
     if(is.null(a)) yy <- cbind(NA,NA,NA,NA,b)
     if(is.null(b)) yy <- cbind(a,NA,NA,NA,NA)
